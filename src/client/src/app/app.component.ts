1003c9239ba72978b306ec2ca7eba6609304ad5d
Cedric Arnould <cedric@sidekickinteractive.com>
2018-01-29 16:37:06 +0000

    Merged PR 422: Merge Setup to master
    Setup the WebApi + Angular App both using SignalR
    Bonus: I added NsWag Studio to configure the Controllers

---
+++ src/client/src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { ValuesService } from './services/api.services';
+import { Observable } from 'rxjs/Observable';
+import { HubConnection } from '@aspnet/signalr-client';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ValuesService],
+})
+export class AppComponent implements OnInit {
+  apiUrl: string = environment.apiUrl;
+
+  //Test API
+  apiValues: string[] = [];
+
+  //Test SignalR
+  private _hubConnection: HubConnection;
+  public async: any;
+  message = '';
+  messages: string[] = [];
+
+  constructor(
+    private valuesService: ValuesService
+  ) {
+    console.log(this.apiUrl);
+  }
+
+  public sendMessage(): void {
+    const data = `Sent: ${this.message}`;
+
+    this._hubConnection.invoke('Send', data);
+    this.messages.push(data);
+  }
+
+  ngOnInit() {
+    //API Initialization
+    this.valuesService.getAll()
+      .subscribe(result => {
+        this.apiValues = result;
+      }, error => {
+        console.log("Error: " + error);
+      });
+
+    //SignalR Initialization
+    this._hubConnection = new HubConnection(this.apiUrl + 'test');
+
+    this._hubConnection.on('Send', (data: any) => {
+      const received = `Received: ${data}`;
+      this.messages.push(received);
+    });
+
+    this._hubConnection.start()
+      .then(() => {
+        console.log('Hub connection started')
+      })
+      .catch(err => {
+        console.log('Error while establishing connection' + err)
+      });
+  }
+}
---
+++ src/server/AspNetCore-SignalR.Api/Hubs/TestHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreSignalR.Api.Hubs
+{
+    public class TestHub : Hub
+    {
+        public Task Send(string data)
+        {
+            return Clients.All.InvokeAsync("Send", data);
+        }
+    }
+}
