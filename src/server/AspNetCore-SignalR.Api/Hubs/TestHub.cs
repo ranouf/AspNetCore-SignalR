7a994ecaae17c0eaa825d1ce923d8ba5f230ad0a
Cedric Arnould <sidekickinteractive@carnould.com>
2018-02-08 11:29:20 -0500

    .

--- src/server/AspNetCore-SignalR.Api/Hubs/TestHub.cs
+++ src/server/AspNetCore-SignalR.Api/Hubs/TestHub.cs
@@ -8,11 +8,6 @@ namespace AspNetCoreSignalR.Api.Hubs
 {
     public class TestHub : Hub
     {
-        public async Task Connect(string groupName)
-        {
-            await Groups.AddAsync(Context.ConnectionId, groupName);
-        }
-
         public Task Send(string data)
         {
             return Clients.All.InvokeAsync("Send", data + " (" + Context.ConnectionId + ")");
